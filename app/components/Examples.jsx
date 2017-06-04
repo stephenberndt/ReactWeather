391312d7b5bd51cdb63fd7a342b8b26ceee4595e
Stephen Berndt <stephen_berndt1@Stephens-MacBook-Air.local>
2017-06-04 19:43:39 -0400

    Added copy

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -4,7 +4,10 @@ var React = require('react');
 
 var Examples = (props) => {
   return (
-    <h3>Examples Component</h3>
+    <div>
+      <h3>Examples Component</h3>
+      <p>Welcome to examples page</p>
+    </div>
   )
 }
 
