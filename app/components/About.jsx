90738ce0fb2928bd9896f52fa4045657f0cef244
Stephen Berndt <stephen_berndt1@Stephens-MacBook-Air.local>
2017-06-04 20:26:56 -0400

    Updated copy in about

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -4,7 +4,11 @@ var React = require('react');
 
 var About = (props) => {
   return (
-    <h3>About Component</h3>
+    <div>
+      <h3>About</h3>
+      <p>More about me</p>
+    </div>
+
   )
 };
 
