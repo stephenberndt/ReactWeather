52fc830673deb8cc96f2c1c42bc5e0b839bc81de
Stephen Berndt <stephen_berndt1@Stephens-MacBook-Air.local>
2017-06-05 01:35:22 -0400

    Add grid system and style samples

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,12 +1,20 @@
 var React = require('react');
-
+var {Link} = require('react-router')
 // Only use this for when a component doesn't use state
 
 var Examples = (props) => {
   return (
     <div>
-      <h3>Examples</h3>
-      <p>Welcome to examples page</p>
+      <h1 className="text-center">Examples</h1>
+      <p>Here are a few example locations to try out:</p>
+      <ol>
+        <li>
+          <Link to='/?location=Dallas,tx'>Dallas, TX</Link>
+        </li>
+        <li>
+          <Link to='/?location=Fairfax,va'>Fairfax, VA</Link>
+        </li>
+      </ol>
     </div>
   )
 }
